6cb963fc6849b1bfef974a1e2b67758acf898c24
Yunjia Xu <jasonxu@umich.edu>
2018-10-29 12:38:58 -0400

    Changes

--- limDest.php
+++ limDest.php
@@ -4,7 +4,6 @@
     
     
     
-    
     $list_four = $dbh->query($sql_query_dest);
     #if(pg_num_rows($list_four) == 0) {
     #   echo "<option value = \"No destinations found for this given source\"></option>";
@@ -12,10 +11,22 @@
     
     echo "<option value = \"" . pg_num_rows($list_four) . "\"></option>";
     
-    while($row_list_dest = $list_four->fetch(PDO::FETCH_ASSOC)):
-        $dest = $row_list_dest["dest"];
+    // while($row_list_dest = $list_four->fetch(PDO::FETCH_ASSOC)):
+    //     $dest = $row_list_dest["dest"];
     
-    //     $sql_query_name="select domain||'(" . $ip .  ")' as domain from serverlookup where " . $ip .  " = '" . $dest . "';";
+    
+    //     $option = "<option value = \"" . $dest . "\"></option>";
+    // echo $option;
+    // endwhile;
+   
+?>
+
+
+
+
+<?php
+
+//     $sql_query_name="select domain||'(" . $ip .  ")' as domain from serverlookup where " . $ip .  " = '" . $dest . "';";
     //     $list_five = $dbh->query($sql_query_name);
     
     // if($row_list_site = $list_five->fetch(PDO::FETCH_ASSOC)) {
@@ -23,8 +34,4 @@
     // }
     
     // $option = "<option value = \"" . $site . "\"></option>";
-        $option = "<option value = \"" . $dest . "\"></option>";
-    echo $option;
-    endwhile;
-   
 ?>
