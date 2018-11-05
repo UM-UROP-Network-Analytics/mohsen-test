7a545110017c01456d07fca457a4ada5b8984f12
Yunjia Xu <jasonxu@umich.edu>
2018-11-05 12:52:35 -0500

    two direction fills

--- index.php
+++ index.php
@@ -373,7 +373,7 @@ function limitDes(str) {
         }
         
         xhttp.send(parameter);
-        if(document.getElementById("browser2").innerHTML != "") {
+        if(document.getElementById("browser1").innerHTML != "") {
            alert("both box filled!");
             //default_time();
         }
@@ -410,7 +410,7 @@ function limitSrc(str) {
         }
         
         xhttp.send(parameter);
-        if(document.getElementById("browser1").innerHTML != "") {
+        if(document.getElementById("browser2").innerHTML != "") {
             alert("both box filled!");
             //default_time();
             //default_time();
