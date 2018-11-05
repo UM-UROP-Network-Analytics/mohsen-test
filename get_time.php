65a7c98d86bc1d08c094786ed02b01ca3102bc76
Yunjia Xu <jasonxu@umich.edu>
2018-11-05 14:02:31 -0500

    two direction fills

--- get_time.php
+++ get_time.php
@@ -3,7 +3,7 @@
     $dest = $_POST["dest"];
     include 'dtb.php';
     $sql_query="select min(min_ts), max(max_ts) from traceroute where src='" . $src . "' and dest='" . $dest . "' group by src, dest;";
-    $list3 = $dbh->query($sql_query_time);
+    $list3 = $dbh->query($sql_query);
     if ($list3 != false) {
         while($row3 = $list3->fetch(PDO::FETCH_ASSOC)):
 
