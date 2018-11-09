92211bf9aa5c40dbd536233e4c4195ccac3fd89e
Yunjia Xu <jasonxu@umich.edu>
2018-11-09 15:02:13 -0500

    added a page

--- result.php
+++ result.php
@@ -125,7 +125,8 @@ body {font-size:16px;}
   <div class="w3-container" id="tracerouteSum" style="margin-top:75px">
     <h1 class="w3-xxlarge w3-text-blue-grey"><b>Traceroute Summary.</b></h1>
     <hr style="width:50px;border:5px solid grey" class="w3-round">
-    <table class="w3-table-all w3-hoverable">
+    <div class="w3-responsive">
+    <table class="w3-table-all w3-hoverable w3-small ">
     <thead>
       <tr class="w3-light-grey">
         <th>Source</th>
@@ -151,7 +152,7 @@ body {font-size:16px;}
 		<?php endwhile; ?>
 	</tbody>
   	</table>
-    
+    </div>
   </div>
   
   <!-- Designers -->
