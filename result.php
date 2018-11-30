6c571fb0b37adb667c8284a2bec5838d6518ac7f
myovan <myovan@umich.edu>
2018-11-29 22:47:43 -0500

    font-size

--- result.php
+++ result.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 <style>
 body,h1,h2,h3,h4,h5 {font-family: "Arial", sans-serif}
-body {font-size:16px;}
+body {font-size:18px;}
 .w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
 .w3-half img:hover{opacity:1}
 </style>
@@ -116,7 +116,6 @@ body {font-size:16px;}
     	(epoch:<?php echo strtotime($_POST["startTime"]); ?>)<br>
     	End time: <?php echo $_POST["endTime"]; ?><br>
     	(epoch:<?php echo strtotime($_POST["endTime"]); ?>)<br>
-    	
     </p>
     
   </div>
